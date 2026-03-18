0c0788d33ab800211a39ed22f964176556534f01
akumar282 <abhi.kumar7567@gmail.com>
2026-03-18 04:23:54 -0700

    when

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 group = "com.spin"
-version = "1.0-SNAPSHOT"
+version = "1.0.0"
 
 repositories {
     mavenCentral()
@@ -18,6 +18,7 @@ tasks.jar {
 
 tasks.shadowJar {
     duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+    archiveFileName.set("tunnel.jar")
 }
 
 dependencies {
